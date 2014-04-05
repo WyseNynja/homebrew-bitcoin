9df5688f2bc6dd51af876669e548418a2de518a6
Tomás Corellano <hack.rapture@gmail.com>
2014-04-05 16:04:09 -0300

    Update czmqpp-gcc48.rb

--- czmqpp-gcc48.rb
+++ czmqpp-gcc48.rb
@@ -2,29 +2,28 @@ require 'formula'
 
 class CzmqppGcc48 < Formula
   homepage 'https://github.com/zeromq/czmqpp'
-  url 'https://github.com/zeromq/czmqpp'
-  sha1 '9e4945b8db0508b0b3d70fc192ed79895a50680d'
-  version '2.2'
+  url 'https://github.com/zeromq/czmqpp.git', :branch => 'master' 
   
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
+  depends_on 'homebrew/versions/gcc48' => :build
   depends_on 'libtool' => :build
   depends_on 'pkg-config' => :build
 
   depends_on 'Nevtep/bitcoin/zeromq2-gcc48'
 
   def install
-    ENV.prepend_path 'PATH', "#{HOMEBREW_PREFIX}/opt/gcc48/bin"
-    ENV['CC'] = "gcc-4.8"
-    ENV['CXX'] = ENV['LD'] = "g++-4.8"
-    ENV.cxx11
+    # ENV.prepend_path 'PATH', "#{HOMEBREW_PREFIX}/opt/gcc48/bin"
+    # ENV['CC'] = "gcc-4.8"
+    # ENV['CXX'] = ENV['LD'] = "g++-4.8"
+    # ENV.cxx11
 
     # I thought depends_on zermoq-gcc48 would be enough, but I guess not...
-    zeromq2gcc48 = Formula.factory('Nevtep/bitcoin/zeromq2-gcc48')
-    ENV.append 'CPPFLAGS', "-I#{zeromq2gcc48.include}"
-    ENV.append 'LDFLAGS', "-L#{zeromq2gcc48.lib}"
+    # zeromq2gcc48 = Formula.factory('Nevtep/bitcoin/zeromq2-gcc48')
+    # ENV.append 'CPPFLAGS', "-I#{zeromq2gcc48.include}"
+    # ENV.append 'LDFLAGS', "-L#{zeromq2gcc48.lib}"
 
-    ENV.cxx11
+    # ENV.cxx11
 
     system "autoreconf", "-i"
 
